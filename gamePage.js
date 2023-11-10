acee2609912d2cbe7857b35ebbdf96f1f2486cee
JoshuaWinfrey <j.winfrey110@gmail.com>
2023-11-10 18:56:00 -0500

    updating css, creating Draft and Player boxes

--- gamePage.js
+++ gamePage.js
@@ -1,3 +1,19 @@
 function initDragula(){
     dragula([document.querySelector('#dragParent')]);
 }
+
+document.addEventListener("DOMContentLoaded", function () {
+    var dragThings = document.querySelectorAll('.dragThing');
+
+    dragThings.forEach(function (dragThing) {
+        dragThing.addEventListener('click', function () {
+            // Remove the 'selected' class from all elements
+            dragThings.forEach(function (element) {
+                element.classList.remove('selected');
+            });
+
+            // Add the 'selected' class to the clicked element
+            dragThing.classList.add('selected');
+        });
+    });
+});
