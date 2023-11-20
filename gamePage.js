ac9754b86966c00ba12b852f3fa8c3094b48183e
JoshuaWinfrey <j.winfrey110@gmail.com>
2023-11-20 18:28:48 -0500

    adding a current team function that declares the current team in Dragula

--- gamePage.js
+++ gamePage.js
@@ -1,4 +1,6 @@
-function initDragula(){
+let currentTeam = null;
+
+function initDragula() {
     dragula([document.querySelector('#dragParent')]);
 }
 
@@ -7,13 +9,36 @@ document.addEventListener("DOMContentLoaded", function () {
 
     dragThings.forEach(function (dragThing) {
         dragThing.addEventListener('click', function () {
-            // Remove the 'selected' class from all elements
-            dragThings.forEach(function (element) {
-                element.classList.remove('selected');
-            });
-
-            // Add the 'selected' class to the clicked element
-            dragThing.classList.add('selected');
+            switchTeam(dragThing);
         });
     });
 });
+
+function switchTeam(clickedTeam) {
+    // Remove the 'selected' class from all elements
+    document.querySelectorAll('.dragThing').forEach(function (element) {
+        element.classList.remove('selected');
+    });
+
+    // Add the 'selected' class to the clicked element
+    clickedTeam.classList.add('selected');
+
+    // Set the currentTeam to the clicked team
+    currentTeam = clickedTeam.textContent.trim(); // Assuming the team name is the text content
+    console.log("Current Team: " + currentTeam);
+}
+
+let addPlayerButton = document.querySelector(".addPlayerButton");
+let player1 = document.querySelector("#player1");
+let playerName = document.querySelector(".playerName");
+
+addPlayerButton.addEventListener("click", selectPlayer);
+
+function selectPlayer() {
+    if (currentTeam) {
+        console.log("You have selected " + playerName.innerHTML + " for " + currentTeam + "!");
+        player1.remove();
+    } else {
+        console.log("Please select a team first.");
+    }
+}
