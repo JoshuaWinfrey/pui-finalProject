fbd92c722fb359c1c4aff1adf173020d4dc6e5aa
JoshuaWinfrey <j.winfrey110@gmail.com>
2023-11-20 19:19:30 -0500

    able to add player to a team via js

--- gamePage.js
+++ gamePage.js
@@ -24,19 +24,43 @@ function switchTeam(clickedTeam) {
     clickedTeam.classList.add('selected');
 
     // Set the currentTeam to the clicked team
-    currentTeam = clickedTeam.textContent.trim(); // Assuming the team name is the text content
+    currentTeam = clickedTeam.textContent.trim(); // The team name is the text content
     console.log("Current Team: " + currentTeam);
 }
 
 let addPlayerButton = document.querySelector(".addPlayerButton");
 let player1 = document.querySelector("#player1");
 let playerName = document.querySelector(".playerName");
+let playerSchool = document.querySelector(".playerSchool");
+let rosterPhoto = document.querySelector(".rosterPhoto");
 
 addPlayerButton.addEventListener("click", selectPlayer);
 
 function selectPlayer() {
     if (currentTeam) {
         console.log("You have selected " + playerName.innerHTML + " for " + currentTeam + "!");
+
+        // Create a new player element
+        let newPlayerElement = document.createElement('div');
+        newPlayerElement.className = 'selectedPlayer';
+
+        // Copy player information to the new player element
+        let playerInfo = document.createElement('div');
+        let playerImage = document.createElement('div');
+        playerInfo.className = 'playerInfo';
+        playerImage.className = 'playerImage';
+        playerInfo.appendChild(playerName.cloneNode(true));
+        playerInfo.appendChild(playerSchool.cloneNode(true));
+        playerImage.appendChild(rosterPhoto.cloneNode(true));
+
+        newPlayerElement.appendChild(playerInfo);
+        newPlayerElement.appendChild(playerImage);
+
+        // Append the new player element to the selected team
+        let selectedTeam = document.querySelector('.selected');
+        selectedTeam.appendChild(newPlayerElement);
+
+        // Remove the original player from the list
         player1.remove();
     } else {
         console.log("Please select a team first.");
