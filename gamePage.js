ea99f3f768c19069406914898e63304e48d2ba0d
JoshuaWinfrey <j.winfrey110@gmail.com>
2023-11-29 17:52:22 -0500

    11/29 updates
    Created function to push all teams, created function to push all players (wip)

--- gamePage.js
+++ gamePage.js
@@ -29,16 +29,18 @@ function switchTeam(clickedTeam) {
 }
 
 let addPlayerButton = document.querySelector(".addPlayerButton");
+let removePlayerButton = document.querySelector(".removePlayerButton");
 let player1 = document.querySelector("#player1");
 let playerName = document.querySelector(".playerName");
 let playerSchool = document.querySelector(".playerSchool");
+let playerPosition = document.querySelector(".playerPosition");
 let rosterPhoto = document.querySelector(".rosterPhoto");
 
 addPlayerButton.addEventListener("click", selectPlayer);
 
 function selectPlayer() {
     if (currentTeam) {
-        console.log("You have selected " + playerName.innerHTML + " for " + currentTeam + "!");
+        let selectedPlayer = document.querySelector('.selected .selectedPlayer');
 
         // Create a new player element
         let newPlayerElement = document.createElement('div');
@@ -49,9 +51,13 @@ function selectPlayer() {
         let playerImage = document.createElement('div');
         playerInfo.className = 'playerInfo';
         playerImage.className = 'playerImage';
-        playerInfo.appendChild(playerName.cloneNode(true));
-        playerInfo.appendChild(playerSchool.cloneNode(true));
-        playerImage.appendChild(rosterPhoto.cloneNode(true));
+        playerPosition.className = ('playerInfo', 'selectedPlayerPosition');
+        playerSchool.className = 'selectedPlayerSchool';
+        playerInfo.appendChild(playerName);
+        playerInfo.appendChild(playerSchool);
+        playerInfo.appendChild(playerPosition);
+        playerImage.appendChild(rosterPhoto);
+        addPlayerButton.remove();
 
         newPlayerElement.appendChild(playerInfo);
         newPlayerElement.appendChild(playerImage);
@@ -59,10 +65,105 @@ function selectPlayer() {
         // Append the new player element to the selected team
         let selectedTeam = document.querySelector('.selected');
         selectedTeam.appendChild(newPlayerElement);
-
-        // Remove the original player from the list
-        player1.remove();
+        console.log(currentTeam + " has selected " + playerName.innerHTML + "!");
+        
     } else {
         console.log("Please select a team first.");
     }
 }
+
+function addTeam(teamName, pick) {
+    let dragParent = document.querySelector('#dragParent');
+
+    let newTeam = document.createElement('div');
+    newTeam.className = 'dragThing';
+
+    let teamNameElement = document.createElement('div');
+    let pickElement = document.createElement('div');
+    teamNameElement.className = 'teamName';
+    pickElement.className = 'pick';
+
+    teamNameElement.textContent = teamName;
+    pickElement.textContent = pick;
+
+    newTeam.appendChild(teamNameElement);
+    newTeam.appendChild(pickElement);
+
+    dragParent.appendChild(newTeam);
+}
+
+addTeam("Chicago Bears", 1);
+addTeam("Arizona Cardinals", 2);
+addTeam("New England Patriots", 3);
+addTeam("Chicago Bears", 4);
+addTeam("Washington Commanders", 5);
+addTeam("New York Giants", 6);
+addTeam("Tampa Bay Buccaneers", 7);
+addTeam("New York Jets", 8);
+addTeam("Los Angeles Chargers", 9);
+addTeam("Tennessee Titans", 10);
+addTeam("Las Vegas Raiders", 11);
+addTeam("New Orleans Saints", 12);
+addTeam("Green Bay Packers", 13);
+addTeam("Los Angeles Rams", 14);
+addTeam("Cincinnati Bengals", 15);
+addTeam("Buffalo Bills", 16);
+addTeam("Arizona Cardinals", 17);
+addTeam("Denver Broncos", 18);
+addTeam("Atlanta Falcons", 19);
+addTeam("Minnesota Vikings", 20);
+addTeam("Indianapolis Colts", 21);
+addTeam("Seattle Seahawks", 22);
+addTeam("Pittsburgh Steelers", 23);
+addTeam("Houstan Texans", 24);
+addTeam("Miami Dolphins", 25);
+addTeam("Dallas Cowboys", 26);
+addTeam("Detroit Lions", 27);
+addTeam("Jacksonville Jaguars", 28);
+addTeam("Kansas City Chiefs", 29);
+addTeam("San Francisco 49ers", 30);
+addTeam("Baltimore Ravens", 31);
+addTeam("Philadelphia Eagles", 32);
+
+function addPlayer(playerName, school, position){
+    let availablePlayers = document.querySelector('.availablePlayers');
+    availablePlayers.className = 'availablePlayers';
+
+    let newPlayer = document.createElement('div');
+    newPlayer.className = 'players';
+
+    let playerNameElement = document.createElement('div');
+    let schoolElement = document.createElement('div');
+    let positionElement = document.createElement('div');
+    let playerPhotoElement = document.createElement('div'); 
+    let rosterPhotoElement = document.createElement('img');
+    rosterPhotoElement.src = "Assets/" + playerName + ".png";
+    
+
+    playerNameElement.className = 'playerName';
+    schoolElement.className = 'playerSchool';
+    positionElement.className = 'playerPosition';
+    rosterPhotoElement.className = 'rosterPhoto';
+
+    playerNameElement.textContent = playerName;
+    schoolElement.textContent = school;
+    positionElement.textContent = position;
+
+    playerPhotoElement.appendChild(rosterPhotoElement);
+    newPlayer.appendChild(playerPhotoElement);
+    newPlayer.appendChild(playerNameElement);
+    newPlayer.appendChild(schoolElement);
+    newPlayer.appendChild(positionElement);
+    
+
+    availablePlayers.appendChild(newPlayer);
+
+}
+
+addPlayer("Drake Maye", "UNC", "QB");
+addPlayer("Marvin Harrison Jr.", "Ohio State", "WR");
+
+
+
+
+
